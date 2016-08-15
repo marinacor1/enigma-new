f2c31504ffd78976eb366300b84ca979e016a5bf
marinacor1 <marinaspam1234@gmail.com>
2016-08-15 09:25:35 -0600

    Offset tests passing

--- test/offset_test.rb
+++ test/offset_test.rb
@@ -10,4 +10,18 @@ class OffsetTest < Minitest::Test
     assert_equal 6, offset.time.to_s.length
   end
 
+  def test_it_returns_last_four_digits
+    offset = Offset.new
+    last_four = offset.create_offset
+    assert_equal 4, last_four.length
+  end
+
+  def test_four_offsets_are_organized
+    offset = Offset.new
+    assert_equal 1, offset.a_offset.length
+    assert_equal 1, offset.b_offset.length
+    assert_equal 1, offset.c_offset.length
+    assert_equal 1, offset.d_offset.length  
+  end
+
 end
