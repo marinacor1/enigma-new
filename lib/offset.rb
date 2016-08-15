89c2344865a9b355b2f834eeab54b49933e2e23e
marinacor1 <marinaspam1234@gmail.com>
2016-08-15 09:18:02 -0600

    Date is an integer of current length

--- lib/offset.rb
+++ lib/offset.rb
@@ -1,7 +1,8 @@
 class Offset
+  attr_accessor :time
 
   def initialize
-    @time = Time.now.strfime('%d%m%y')
+    @time = Time.now.strftime('%d%m%y').to_i
   end
 
 end
--- test/offset_test.rb
+++ test/offset_test.rb
@@ -1,3 +1,4 @@
+require 'pry'
 require 'minitest/autorun'
 require 'minitest/pride'
 require_relative '../lib/offset'
@@ -5,8 +6,8 @@ require_relative '../lib/offset'
 class OffsetTest < Minitest::Test
   def test_it_returns_a_date_that_is_six_digits
     offset = Offset.new
-    assert_equal "Float", offset.time.class
-    assert_equal 6, offset.time.length
+    assert_equal Fixnum, offset.time.class
+    assert_equal 6, offset.time.to_s.length
   end
 
 end
