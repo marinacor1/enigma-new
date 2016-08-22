9122c61c96d59c0e533586af24e951bf47ec1a57
marinacor1 <marinaspam1234@gmail.com>
2016-08-21 19:48:23 -0600

    Refactor key class

--- lib/key_generator.rb
+++ lib/key_generator.rb
@@ -1,12 +1,8 @@
 class KeyGenerator
-  attr_accessor :key, :a_rotation, :b_rotation, :c_rotation, :d_rotation
+  attr_accessor :key
 
   def initialize
     @key = random_key
-    @a_rotation = @key[0] + @key[1]
-    @b_rotation = @key[1] + @key[2]
-    @c_rotation = @key[2] + @key[3]
-    @d_rotation = @key[3] + @key[4]
   end
 
   def random_key
--- test/key_generator_test.rb
+++ test/key_generator_test.rb
@@ -13,14 +13,6 @@ class KeyGeneratorTest < MiniTest::Test
   def test_each_key_is_five_digits
     keygenerator = KeyGenerator.new
     assert_equal 5, keygenerator.key.length
-    # assert_equal Fixnum, keygenerator.key.class
   end
 
-  def test_rotations_are_two_characters_in_length
-    keygenerator = KeyGenerator.new
-    assert_equal 2, keygenerator.a_rotation.length
-    assert_equal 2, keygenerator.b_rotation.length
-    assert_equal 2, keygenerator.c_rotation.length
-    assert_equal 2, keygenerator.d_rotation.length
-  end
 end
